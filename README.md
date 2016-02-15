f592fe4afcc7872fc8f78f569128c7a73b175af6
Tommy DANGerous <quantumventuress@gmail.com>
2016-02-15 07:02:39 -0800

    [tommy] Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Server Rendering
+
+## Development
+
+Install Node packages
+```sh
+$ npm install
+```
+
+Start development server
+```sh
+$ npm start
+```
+
+## Production
+
+Compile to from ES6 to ES5
+```sh
+$ npm run build
+```
+
+Run production server
+```sh
+$ npm run production
+```
---
+++ app/server.js
@@ -0,0 +1,26 @@
+// Libs
+import express from 'express';
+import morgan from 'morgan';
+
+// Routes
+import routes from './routes/routes';
+
+// Environment
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// Port
+const port = process.env.PORT || 8080;
+
+// Express App
+const app = express();
+
+// Logging
+app.use(morgan('combined'));
+
+// Routes
+app.use('/', routes);
+
+// Start server
+const server = app.listen(port, () => {
+  const { address, port } = server.address();
+  console.log(`Server running in ${process.env.NODE_ENV} at http://${address}:${port}`);
+})
